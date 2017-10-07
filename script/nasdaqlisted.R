813ffc34d53ac03eaaf49abdbd0f200675116889
Marco Repetto <marcorepetto@mbp-di-marco.home>
2017-10-07 21:58:53 +0200

    last commit optimized

--- script/nasdaqlisted.R
+++ script/nasdaqlisted.R
@@ -2,10 +2,6 @@
 #load the necessary libraries
 require(RCurl)
 
-#function enunciation
-commonstock <- function(secname){
-return(grep("Common Stock", secname))
-}
 
 #searching for the NASDAQ listed companies
 txt=getURL("ftp://nasdaqtrader.com/SymbolDirectory/Nasdaqlisted.txt", ssl.verifypeer = FALSE)
@@ -15,24 +11,17 @@ listdata = read.table(textConnection(txt), header = TRUE, sep = "|", dec = "\n" 
 
 #take only the common stocks
 #for cycle
-liststock <- data.frame(listdata[0,])
-#k=1
-#i=1
-#for(i in 1:(length(listdata$Security.Name))){
-#	
-#	if ((length(grep("Common Stock", listdata$Security.Name[i]))) == 1){
-#		
-#		liststock[k,] = listdata[i,]
-#		k = k + 1
-#	}
-#}
+liststock <- listdata[,1]
 
-lapply(listdata$Security.Name, commonstock)
+	
+n <- grep("Common Stock", listdata$Security.Name)
+liststock <- liststock[n]
 
 #create an array of symbols
-#symbols = as.character(liststock$Symbol)
+symbols = as.character(liststock)
 
 #eliminate the last element (data update blah)
-#symbols <- head(symbols, -2)
+symbols <- head(symbols, -2)
 
+print(symbols)
 
