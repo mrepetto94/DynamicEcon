4c3bbb30274324e8f45fc41d7fdac3477a8d3a1f
Marco Repetto <mrepetto94@me.com>
2017-10-08 15:59:45 +0200

    improvement

--- script/main.R
+++ script/main.R
@@ -4,12 +4,18 @@ require(alphavantager)
 
 av_api_key("JJQ9PELUQJJ2FQ0U")
 
-set.seed(2)
+set.seed(60)
 
 readSym <- function(sym) {
     out <- tryCatch(
         {
          x <- av_get(symbol = sym, av_fun = "TIME_SERIES_INTRADAY", interval = "1min") 
+	if(length(x$close) == 100){
+    	return(x$close)
+	}else{
+	return(NA)
+
+	}
 	},
         error=function(cond) {
             message(paste("URL does not seem to exist:", sym))
@@ -23,27 +29,23 @@ readSym <- function(sym) {
             message(paste("Processed URL:", sym))
         }
     )    
+return(out)
 
-if(length(x$close) == 100){
-    return(x$close)
-}else{
-return(NA)
-}
 }
 
-list <- source("nasdaqlisted.R")
-list <- list$value
+list <- read.table("listedCS.csv", sep = ";")
+list <- as.character(list[,1])
 
 #just to let the first warning go away
 f <- av_get(symbol = list[1], av_fun = "TIME_SERIES_INTRADAY", interval = "1min") 
 
-list <- sample(list, 10)
+list <- sample(list, 34)
 
 stock <- sapply(list, readSym)
 
 stock <-as.data.frame(stock)
 
-write.table(stock, file = "stock.csv", dec=",", sep = ";")
+write.table(stock, file = "stock.csv", dec=",", sep = ";", row.names = FALSE)
 
 
 
