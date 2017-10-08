0b5a6aa96dcf0be9183c47af3661b6f8eae0b539
Marco Repetto <marcorepetto@mbp-di-marco.home>
2017-10-08 11:24:29 +0200

    major change and data frame implementation

--- script/main.R
+++ script/main.R
@@ -4,30 +4,45 @@ require(alphavantager)
 
 av_api_key("JJQ9PELUQJJ2FQ0U")
 
+set.seed(2)
+
 readSym <- function(sym) {
     out <- tryCatch(
         {
-            av_get(symbol = sym, av_fun = "TIME_SERIES_INTRADAY", interval = "1min") 
-        },
+         x <- av_get(symbol = sym, av_fun = "TIME_SERIES_INTRADAY", interval = "1min") 
+	},
         error=function(cond) {
             message(paste("URL does not seem to exist:", sym))
             return(NA)
         },
         warning=function(cond) {
             message(paste("URL caused a warning:", sym))
-            return(NULL)
+            return(NA)
         },
         finally={ 
             message(paste("Processed URL:", sym))
         }
     )    
-    return(out)
+
+if(length(x$close) == 100){
+    return(x$close)
+}else{
+return(NA)
+}
 }
 
 list <- source("nasdaqlisted.R")
 list <- list$value
 
-list <- sample(list, 2)
+#just to let the first warning go away
+f <- av_get(symbol = list[1], av_fun = "TIME_SERIES_INTRADAY", interval = "1min") 
 
-sapply(list, readSym)
+list <- sample(list, 100)
+
+stock <- sapply(list, readSym)
+
+stock <-as.data.frame(stock)
+
+
+
 
