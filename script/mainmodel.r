615beceb65163617fa37968cc47bc220cff340bf
Marco Repetto <mrepetto94@me.com>
2017-12-11 19:03:02 +0100

    Some simplification

--- script/mainmodel.r
+++ script/mainmodel.r
@@ -11,44 +11,67 @@ library(neuralnet)
 set.seed(60)
 s <- sample(1:31, 1)
 
-#get the stock value
-stock <- read.table("stock.csv", dec = ",", sep = ";", header = 1)
+#Start from the  first sixty data points
+stockfull <- read.table("stock.csv", dec = ",", sep = ";", header = 1)
 
-#get training set
-train <- ts(stock[1:60,s])
+i <- 1
+buy <- FALSE
+#starting a loop that will loop 30 times
+for (i in 1:30){
 
-#fit model
-fit <- auto.arima(train)
+  print("starting round")
+  print(i)
+  
+  stock <- stockfull[i:(i+59),]
+  
+  if (i != 1){
+    # buying side
+    if (buy == TRUE){
+      if pred 
+    }
+    # selling side
+  }
+  
+  
+  
+  
+  #get training set
+  train <- ts(stock[,s])
+  
+  #fit model
+  fit <- auto.arima(train)
 
-#get fitting error
-error <- train * 0
-error <- fit$fitted - train
+  #get fitting error
+  error <- train * 0
+  error <- fit$fitted - train
 
-#NN part
-#get out the first error element
-errortrim <- error[-c(1)]
+  #NN part
+  #get out the first error element a last element of train
+  error.trim <- error[-c(1)]
+  stock.trim <- stock[-c(length(train)),]
 
-#get a normalized dataframe of the stock price
-scaled <-as.data.frame(scale(train))
+  #get a normalized dataframe of the stock price
+  scaled <-as.data.frame(scale(stock))
+  scaled.trim <-as.data.frame(scale(stock.trim))
 
-names(scaled)[s] <- paste(names(train[s]), "error", sep = "")
-train[s] <- as.numeric(as.character(unlist(error)))
-n <- names(train)
+  scaled.trim[s] <- as.numeric(as.character(unlist(error.trim)))
+  n <- names(scaled)
 
-#try to train a nnet on the stock's error using the prices of the other equity
-y <- paste(n[s], "~")
+  #try to train a nnet on the stock's error using the prices of the other equity
+  y <- paste(n[s], "~")
+  f <- as.formula(paste(y, paste(n[!n %in% n[s]], collapse = " + ")))
 
-f <- as.formula(paste(y, paste(n[!n %in% n[s]], collapse = " + ")))
+  nn <- neuralnet(f,data=scaled.trim,hidden=c(20),linear.output=T)
 
-nn <- neuralnet(f,data=train,hidden=c(16),linear.output=T)
-
-#try to predict it 
-foreca.nn <- compute(nn,stockscaled[60, -s])$net.result
-
-#convert it to a strategy
-
-
-#add the naive strategy
-naive <- sample(c(0,1), replace=TRUE, size=length(strategy))
-
-
+  #adjust ARIMA prediction
+  foreca <- forecast(fit, h = 1)
+  pred <- as.numeric(compute(nn,scaled[60, -s])$net.result) + as.numeric(foreca$mean)
+  
+  #convert prediction to signal
+  signal.model <- 0
+  if (pred > stock[60,s]){
+    signal.model <- 1
+  }
+  signal.naive <- sample(c(0,1), replace=TRUE, size=1)
+  
+}
